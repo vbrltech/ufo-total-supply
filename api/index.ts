21db4ec173f132d1062012f383a6383431bf2805
manylovv <yellowmanylov@gmail.com>
2024-04-22 17:34:28 +0500

    feat: init

---
+++ api/index.ts
@@ -0,0 +1,18 @@
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+
+export const config = {
+  runtime: 'edge',
+};
+
+const app = new Hono().basePath('/api');
+
+app.get('/tgram/total', (c) => {
+  return c.json(210000000000);
+});
+
+app.get('/api/tgram/circulating', (c) => {
+  return c.json(209999999998);
+});
+
+export default handle(app);
