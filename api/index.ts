2185da5fffea46bddcd78e425c4707580bddaf02
Artash <artashes@vbrl.ai>
2024-08-26 11:12:28 +0300

    Init

--- api/index.ts
+++ api/index.ts
@@ -7,12 +7,12 @@ export const config = {
 
 const app = new Hono().basePath('/api');
 
-app.get('/tgram/total', (c) => {
-  return c.json(210000000000);
+app.get('/total', (c) => {
+  return c.json(1000000000);
 });
 
-app.get('/tgram/circulating', (c) => {
-  return c.json(209999999998);
+app.get('/circulating', (c) => {
+  return c.json(999987333);
 });
 
 export default handle(app);
