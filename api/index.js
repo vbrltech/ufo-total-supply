57cc1bfad95319544758f58c6528b6ee2784276e
Artash <artashes@vbrl.ai>
2024-08-28 10:48:59 +0300

    Get total and circulating from solscan

--- api/index.js
+++ api/index.js
@@ -1,14 +1,29 @@
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
+import { Connection, PublicKey } from '@solana/web3.js';
+const connection = new Connection('https://api.mainnet-beta.solana.com');
+const tokenMintAddress = new PublicKey('99myne1mVnsSv2kAkzZDfNnwf5Xb4SASJnaCRXdbpump');
 
 const app = new Hono().basePath('/api');
 
-app.get('/total', (c) => {
+async function getTotalAmount() {
+  const supply = await connection.getTokenSupply(tokenMintAddress);
+  return supply.value.uiAmount;
+}
+
+app.get('/max', (c) => {
   return c.json(1000000000);
 });
 
-app.get('/circulating', (c) => {
-  return c.json(999987333);
+app.get('/total', async (c) => {
+  const supply = await getTotalAmount();
+  return c.json(supply);
+});
+
+
+app.get('/circulating', async (c) => {
+  const supply = await getTotalAmount();
+  return c.json(supply);
 });
 
 serve(app)
